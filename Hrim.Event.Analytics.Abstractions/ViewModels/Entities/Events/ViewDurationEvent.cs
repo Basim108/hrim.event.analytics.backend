b8e096607b236985e5c4668b788b389bdc2215e7
Basim Al-Jawahery <basimdev@gmail.com>
2023-02-26 21:59:19 +0400

    feat: add event-type entity in the event view model (#14)

--- Hrim.Event.Analytics.Abstractions/ViewModels/Entities/Events/ViewDurationEvent.cs
+++ Hrim.Event.Analytics.Abstractions/ViewModels/Entities/Events/ViewDurationEvent.cs
@@ -1,18 +1,14 @@
+using Hrim.Event.Analytics.Abstractions.ViewModels.Entities.EventTypes;
+
 namespace Hrim.Event.Analytics.Abstractions.ViewModels.Entities.Events;
 
 /// <summary>
 ///     When it is important to register an event that has start time and end time this system_event_type can be used.
 ///     <br />This kind of events may occur several times a day.
 /// </summary>
+/// <param name="Id">Event id</param>
 /// <param name="StartedAt">Date and time with end-user timezone when en event starts</param>
 /// <param name="FinishedAt">Date and time with end-user timezone when an event finishes</param>
-/// <param name="Id">Event id</param>
-/// <param name="EventTypeId">Id of event type on which this event is based on</param>
-/// <param name="Color">color from event type</param>
+/// <param name="EventType">Event type on which this event is based on</param>
 /// <param name="ConcurrentToken">Update is possible only when this token equals to the token in the storage</param>
-public record ViewDurationEvent(Guid Id,
-    DateTimeOffset StartedAt,
-    DateTimeOffset? FinishedAt,
-    Guid EventTypeId,
-    string Color,
-    long ConcurrentToken);
+public record ViewDurationEvent(Guid Id, DateTimeOffset StartedAt, DateTimeOffset? FinishedAt, ViewEventType EventType, long ConcurrentToken);
--- Hrim.Event.Analytics.EfCore/Cqrs/Events/DurationEventGetForPeriodHandler.cs
+++ Hrim.Event.Analytics.EfCore/Cqrs/Events/DurationEventGetForPeriodHandler.cs
@@ -1,44 +1,40 @@
 using Hrim.Event.Analytics.Abstractions.Cqrs.Events;
 using Hrim.Event.Analytics.Abstractions.Extensions;
 using Hrim.Event.Analytics.Abstractions.ViewModels.Entities.Events;
+using Hrim.Event.Analytics.Abstractions.ViewModels.Entities.EventTypes;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
 
 namespace Hrim.Event.Analytics.EfCore.Cqrs.Events;
 
-public class DurationEventGetForPeriodHandler: IRequestHandler<DurationEventGetForPeriod, IList<ViewDurationEvent>> {
+public class DurationEventGetForPeriodHandler: IRequestHandler<DurationEventGetForPeriod, IList<ViewDurationEvent>>
+{
     private readonly EventAnalyticDbContext _context;
 
-    public DurationEventGetForPeriodHandler(EventAnalyticDbContext context) {
-        _context = context;
-    }
+    public DurationEventGetForPeriodHandler(EventAnalyticDbContext context) { _context = context; }
 
     public async Task<IList<ViewDurationEvent>> Handle(DurationEventGetForPeriod request, CancellationToken cancellationToken) {
         var dbEntities = await _context.DurationEvents
                                        .Include(x => x.EventType)
-                                       .Where(x => x.CreatedById == request.Context.UserId &&
-                                                   (x.StartedOn <= request.Start && x.FinishedOn > request.Start ||
-                                                    x.StartedOn >= request.Start && x.StartedOn  <= request.End  ||
-                                                    x.StartedOn < request.End    && x.FinishedOn >= request.End) &&
-                                                   x.IsDeleted != true)
+                                       .Where(x => x.CreatedById == request.Context.UserId
+                                                && (x.StartedOn <= request.Start && x.FinishedOn > request.Start
+                                                 || x.StartedOn >= request.Start && x.StartedOn  <= request.End
+                                                 || x.StartedOn < request.End    && x.FinishedOn >= request.End)
+                                                && x.IsDeleted != true)
                                        .AsNoTracking()
-                                       .Select(x => new {
-                                            x.Id,
-                                            x.StartedOn,
-                                            x.StartedAt,
-                                            x.FinishedOn,
-                                            x.FinishedAt,
-                                            x.EventTypeId,
-                                            x.EventType!.Color,
-                                            x.ConcurrentToken
-                                        })
                                        .ToListAsync(cancellationToken);
-        var result = dbEntities.Select(x => new ViewDurationEvent(x.Id,
-                                                                  x.StartedOn.CombineWithTime(x.StartedAt),
-                                                                  x.FinishedOn?.CombineWithTime(x.FinishedAt!.Value),
-                                                                  x.EventTypeId,
-                                                                  x.Color,
-                                                                  x.ConcurrentToken))
+        var result = dbEntities.Select(x
+                                           => new ViewDurationEvent(x.Id,
+                                                                    x.StartedOn.CombineWithTime(x.StartedAt),
+                                                                    x.FinishedOn?.CombineWithTime(x.FinishedAt!.Value),
+                                                                    new ViewEventType(x.EventType!.Id,
+                                                                                      x.EventType.Name,
+                                                                                      x.EventType.Description,
+                                                                                      x.EventType.Color,
+                                                                                      x.EventType.IsPublic,
+                                                                                      x.EventType.IsDeleted ?? false,
+                                                                                      true),
+                                                                    x.ConcurrentToken))
                                .ToList();
         return result;
     }
