c4c5a845ec5519752b654a94ae3ed07c77a31443
Basim Al-Jawahery <basimdev@gmail.com>
2023-07-04 22:04:04 +0400

    HC-130 feat(db): add db migration with gap analysis feature

--- Hrim.Event.Analytics.Abstractions/FeatureCodes.cs
+++ Hrim.Event.Analytics.Abstractions/FeatureCodes.cs
@@ -6,7 +6,7 @@ namespace Hrim.Event.Analytics.Abstractions;
 public static class FeatureCodes
 {
     public const string COUNT_ANALYSIS = "count";
-    public const string GAP_ANALYSIS = "gap";
+    public const string GAP_ANALYSIS   = "gap";
 
     public static readonly ImmutableList<string> AllCodes = ImmutableList.Create(COUNT_ANALYSIS,
                                                                                  GAP_ANALYSIS);
--- Hrim.Event.Analytics.EfCore/DbConfigurations/HrimFeatureDbConfig.cs
+++ Hrim.Event.Analytics.EfCore/DbConfigurations/HrimFeatureDbConfig.cs
@@ -1,4 +1,6 @@
+using Hrim.Event.Analytics.Abstractions;
 using Hrim.Event.Analytics.Abstractions.Entities;
+using Hrim.Event.Analytics.Abstractions.Enums;
 using Hrimsoft.StringCases;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
@@ -8,12 +10,12 @@ namespace Hrim.Event.Analytics.EfCore.DbConfigurations;
 public class HrimFeatureDbConfig: IEntityTypeConfiguration<HrimFeature>
 {
     public void Configure(EntityTypeBuilder<HrimFeature> builder) {
-        builder.ToTable(name: "hrim_features", 
+        builder.ToTable(name: "hrim_features",
                         t => t.HasComment(comment: @"Features that might be on/off;
 for example, analysis based on event-types, tags, events, etc"));
 
         builder.AddEntityProperties();
-        
+
         builder.Property(p => p.FeatureType)
                .HasColumnName(nameof(HrimFeature.FeatureType).ToSnakeCase())
                .HasConversion<string>()
@@ -23,21 +25,33 @@ Analysis")
 
         builder.Property(p => p.Description)
                .HasColumnName(nameof(HrimFeature.Description).ToSnakeCase());
-        
+
         builder.Property(p => p.Code)
                .HasColumnName(nameof(HrimFeature.Code).ToSnakeCase())
                .HasComment("Feature code")
                .IsRequired();
-        
+
         builder.Property(p => p.VariableName)
                .HasColumnName(nameof(HrimFeature.VariableName).ToSnakeCase())
                .HasComment("Environment variable name that controls is this feature set on/off")
                .IsRequired();
-                
+
         builder.Property(p => p.IsOn)
                .HasColumnName(nameof(HrimFeature.IsOn).ToSnakeCase())
                .HasComment(@"When a feature is off then its hangfire jobs, in case existed, should not be proceeded or scheduled.
 and in case feature represents an analysis (e.g. count, gap) this analysis should not appear in the list of available analysis.")
                .IsRequired();
+
+        builder.HasData(new HrimFeature {
+            Id              = Guid.Parse("2F1E83AA-A0F2-492F-AF76-C6DE43AD277B"),
+            Code            = FeatureCodes.GAP_ANALYSIS,
+            VariableName    = "FEAT_GAP_ANALYSIS",
+            FeatureType     = FeatureType.Analysis,
+            Description     = "Calculates gaps between events of a specific event type",
+            IsOn            = true,
+            CreatedAt       = DateTime.Parse("2023-06-21 21:02:52.864000 +00:00"),
+            UpdatedAt       = DateTime.Parse("2023-06-21 21:02:52.864000 +00:00"),
+            ConcurrentToken = 1
+        });
     }
 }
